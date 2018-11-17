068fae5c41283405b7dd30a41514affb8d81dda7
KarlChuang <b05901043@ntu.edu.tw>
2018-11-17 23:39:20 +0800

    best is model_6

--- hw3/train.py
+++ hw3/train.py
@@ -81,13 +81,9 @@ class Model():
         use Keras to build a network
         """
         # Convolutional layers
-        self.model.add(Conv2D(filters=64, kernel_size=(5, 5), strides=(1, 1),
+        self.model.add(Conv2D(filters=128, kernel_size=(5, 5), strides=(1, 1),
                               activation='relu',
                               input_shape=(48, 48, 1)))
-        self.model.add(MaxPooling2D(pool_size=(2, 2)))
-        self.model.add(Dropout(0.5))
-        self.model.add(Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1),
-                              activation='relu'))
         self.model.add(BatchNormalization())
         self.model.add(Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1),
                               activation='relu'))
@@ -97,6 +93,13 @@ class Model():
                               activation='relu'))
         self.model.add(BatchNormalization())
         self.model.add(Conv2D(filters=256, kernel_size=(2, 2), strides=(1, 1),
+                              activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.5))
+        self.model.add(Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1),
+                              activation='relu'))
+        self.model.add(BatchNormalization())
+        self.model.add(Conv2D(filters=512, kernel_size=(2, 2), strides=(1, 1),
                               activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         self.model.add(Dropout(0.5))
@@ -115,7 +118,7 @@ class Model():
         """
         compile the model
         """
-        sgd = SGD(lr=0.005, decay=0.00001, momentum=0.9)
+        sgd = SGD(lr=0.01, decay=0.00001, momentum=0.9)
         self.model.compile(
             loss='categorical_crossentropy',
             optimizer=sgd,
