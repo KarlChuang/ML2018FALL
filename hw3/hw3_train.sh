162adc50860ae20e0afb14ff8a09380e1a33b7bd
KarlChuang <b05901043@ntu.edu.tw>
2018-11-30 16:20:55 +0800

    Add report

--- hw3/hw3_train.sh
+++ hw3/hw3_train.sh
@@ -1,5 +1,5 @@
 if [ "$(python3 --version | head -n1 | cut -d " " -f2 | cut -d "." -f2)" == 6 ]; then
-    python3 train.py $1 $2
+    python3 train.py $1 ./model/model_final.hs
 else
-    python3.6 train.py $1 $2
+    python3.6 train.py $1 ./model/model_final.hs
 fi
