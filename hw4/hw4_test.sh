aad05ae7ba6a8b8969ee997cea091553b05c5791
KarlChuang <b05901043@ntu.edu.tw>
2018-12-20 20:25:17 +0800

    add dropbox link for model downloading

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,1 +1,7 @@
+mkdir model
+wget 'https://www.dropbox.com/s/hsue9yllgy7o3oe/model_9.model?dl=1'
+mv model_9.model?dl=1 ./model/model_9.model
+wget 'https://www.dropbox.com/s/amypm597xzq08r9/embedding250.model?dl=1'
+mv embedding250.model?dl=1 ./model/embedding250.model
+
 python3.6 test_final.py ./model/model_9.model $1 $2 $3
